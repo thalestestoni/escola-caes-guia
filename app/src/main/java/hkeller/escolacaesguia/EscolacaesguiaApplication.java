79f836a909071690b1b8033cd65d5a9de8ae1811
Thales F Testoni <thalestestoni@gmail.com>
2023-03-04 15:29:03 -0300

    Setup inicial Java + Sprig

---
+++ app/src/main/java/hkeller/escolacaesguia/EscolacaesguiaApplication.java
@@ -0,0 +1,13 @@
+package hkeller.escolacaesguia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolacaesguiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolacaesguiaApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/hkeller/escolacaesguia/EscolacaesguiaApplicationTests.java
@@ -0,0 +1,13 @@
+package hkeller.escolacaesguia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolacaesguiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
