6883e47ddc8edf41c90d75a2d2cf4ae0bb4ef014
Thales F Testoni <thalestestoni@gmail.com>
2023-04-03 08:14:22 -0300

    Removido classes de login inutlizadas

--- app/src/main/java/hkeller/escolacaesguia/controllers/LoginController.java
+++ app/src/main/java/hkeller/escolacaesguia/controllers/LoginController.java
@@ -2,16 +2,9 @@ package hkeller.escolacaesguia.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import hkeller.escolacaesguia.services.LoginService;
 
 @Controller
 public class LoginController {
-
-    final LoginService loginService;
-
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
-    }
 
     @GetMapping("/login")
     public String loginForm() {
