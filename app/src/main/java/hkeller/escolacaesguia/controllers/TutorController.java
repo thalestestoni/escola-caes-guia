031da2abcd2949bb6dfa7fae34e779b7479fa998
Thales Flores Testoni <thales.testoni@havan.com.br>
2023-05-08 17:26:54 -0300

    Visualização de tutor

--- app/src/main/java/hkeller/escolacaesguia/controllers/TutorController.java
+++ app/src/main/java/hkeller/escolacaesguia/controllers/TutorController.java
@@ -6,11 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import hkeller.escolacaesguia.dtos.CadastroTutorDto;
+import hkeller.escolacaesguia.dtos.UsuarioDto;
 import hkeller.escolacaesguia.services.CadastrarTutorServico;
+import hkeller.escolacaesguia.services.ObterTutorServico;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -21,6 +24,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class TutorController {
     @Autowired
     CadastrarTutorServico cadastrarTutorServico;
+
+    @Autowired
+    ObterTutorServico obterTutorServico;
+
 
     @GetMapping("cadastro")
     public String getFormularioCadastro(Model model) {
@@ -55,5 +62,14 @@ public class TutorController {
         model.addAttribute("baseUrl", baseUrl);
 
         return "tutor/listagem";
+    }
+
+    @GetMapping("{idTutor}/editar")
+    public String formEditar(@PathVariable("idTutor") Long idTutor, Model model) {
+        UsuarioDto tutorDto = obterTutorServico.execute(idTutor);
+
+        model.addAttribute("tutor", tutorDto);
+        
+        return "tutor/editar";
     }
 }
