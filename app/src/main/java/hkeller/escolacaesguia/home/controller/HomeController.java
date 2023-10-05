1ff1b168dd09d1eeaac8cda92a47b1e9879f73eb
Thales Flores Testoni <thales.testoni@havan.com.br>
2023-10-05 19:39:45 -0300

    Exemplo de uso enums e serviço endereco

--- app/src/main/java/hkeller/escolacaesguia/home/controller/HomeController.java
+++ app/src/main/java/hkeller/escolacaesguia/home/controller/HomeController.java
@@ -1,7 +1,11 @@
 package hkeller.escolacaesguia.home.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hkeller.escolacaesguia.common.enums.EstadoCivilEnum;
+import hkeller.escolacaesguia.common.enums.GrauInstrucaoEnum;
 
 @Controller
 public class HomeController {
@@ -11,7 +15,9 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("grausInstrucao", GrauInstrucaoEnum.values());
+        model.addAttribute("estadosCivis", EstadoCivilEnum.values());
         return "home";
     }
 }
